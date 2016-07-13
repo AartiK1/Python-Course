085c4be8d1e2ba5f14c8239869f1ca10710b32fe
Aarti Khatri <aarti.khatri@hotmail.co.uk>
2016-07-13 15:54:34 +0100

    Ex7 Complete

--- ex7.py
+++ ex7.py
@@ -1,8 +1,9 @@
-print "Mary had a little lamb."
-print "Its fleece was white as %s." % 'snow'
+print "Mary had a little lamb." 
+print "Its fleece was white as %s." % 'snow' #inserts snow in place of %s
 print "And everywhere that Mary went."
-print "." * 10  # what'd that do?
+print "." * 10  # what'd that do? It repeats . 10 times in a row
 
+#defines variables
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,5 +18,5 @@ end11 = "e"
 end12 = "r"
 
 # watch that comma at the end.  try removing it to see what happens
-print end1 + end2 + end3 + end4 + end5 + end6,
+print end1 + end2 + end3 + end4 + end5 + end6, #the comma ensures the next print line is printed on the same line. removing the comma prints Cheese on 1 line and Burger on a line below 
 print end7 + end8 + end9 + end10 + end11 + end12
