3f77e169e3e600bd52204f33d93fb5db2f4c2582
Aarti Khatri <aarti.khatri@hotmail.co.uk>
2016-07-14 13:54:30 +0100

    Ex13 Complete

---
+++ ex13.1.py
@@ -0,0 +1,11 @@
+from sys import argv #argv is an argument variable from  the sys module/library.
+
+script, first, second = argv
+
+first = raw_input("How old are you? ")
+second = raw_input("What's your name? ")
+
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
---
+++ ex13.2.py
@@ -0,0 +1,15 @@
+from sys import argv #argv is an argument variable from  the sys module/library.
+
+script, first, second, third, fourth = argv
+
+first = raw_input("How old are you? ")
+second = raw_input("What's your name? ")
+first = raw_input("What country are you from? ")
+second = raw_input("What colour eyes do you have? ")
+
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", first
+print "Your fourth variable is:", second
